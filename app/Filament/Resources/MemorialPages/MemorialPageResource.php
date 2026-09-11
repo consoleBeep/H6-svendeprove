6a0bc1e389fe6d0d57950d61fdd763e4ddd2cb7b
consoleBeep <casperamadsen@gmail.com>
2026-09-11 20:47:00 +0200

    feat: manage photos from the memorial page resource

--- app/Filament/Resources/MemorialPages/MemorialPageResource.php
+++ app/Filament/Resources/MemorialPages/MemorialPageResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\MemorialPages;
 use App\Filament\Resources\MemorialPages\Pages\CreateMemorialPage;
 use App\Filament\Resources\MemorialPages\Pages\EditMemorialPage;
 use App\Filament\Resources\MemorialPages\Pages\ListMemorialPages;
+use App\Filament\Resources\MemorialPages\RelationManagers\PhotosRelationManager;
 use App\Filament\Resources\MemorialPages\Schemas\MemorialPageForm;
 use App\Filament\Resources\MemorialPages\Tables\MemorialPagesTable;
 use App\Models\MemorialPage;
@@ -33,7 +34,7 @@ class MemorialPageResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            PhotosRelationManager::class,
         ];
     }
 
