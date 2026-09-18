1e6d0019974f889befa211e9a91ae77b9bfef3af
consoleBeep <casperamadsen@gmail.com>
2026-09-18 17:22:00 +0200

    fix: update logout test after removing the old navigation menu

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -54,31 +54,12 @@ class AuthenticationTest extends TestCase
         $this->assertGuest();
     }
 
-    public function test_navigation_menu_can_be_rendered(): void
-    {
-        $user = User::factory()->create();
-
-        $this->actingAs($user);
-
-        $response = $this->get('/dashboard');
-
-        $response
-            ->assertOk()
-            ->assertSeeVolt('layout.navigation');
-    }
-
     public function test_users_can_logout(): void
     {
         $user = User::factory()->create();
 
-        $this->actingAs($user);
-
-        $component = Volt::test('layout.navigation');
-
-        $component->call('logout');
-
-        $component
-            ->assertHasNoErrors()
+        $this->actingAs($user)
+            ->post('/logout')
             ->assertRedirect('/');
 
         $this->assertGuest();
