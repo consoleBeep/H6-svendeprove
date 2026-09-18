70a305522b1d9500012b16c9773ffc1a61504071
consoleBeep <casperamadsen@gmail.com>
2026-09-18 16:15:00 +0200

    fix: add missing relations to the user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,13 @@ use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
@@ -20,11 +23,6 @@ class User extends Authenticatable implements FilamentUser
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -38,6 +36,13 @@ class User extends Authenticatable implements FilamentUser
         return $this->role === 'admin';
     }
 
+    protected function avatarUrl(): Attribute
+    {
+        return Attribute::get(fn (): ?string => $this->avatar_path
+            ? Storage::disk('public')->url($this->avatar_path)
+            : null);
+    }
+
     public function canAccessPanel(Panel $panel): bool
     {
         return $this->isAdmin();
@@ -46,5 +51,25 @@ class User extends Authenticatable implements FilamentUser
     public function memorialPages(): HasMany
     {
         return $this->hasMany(MemorialPage::class);
+    }
+
+    public function administeredMemorialPages(): BelongsToMany
+    {
+        return $this->belongsToMany(MemorialPage::class)->withTimestamps();
+    }
+
+    public function memories(): HasMany
+    {
+        return $this->hasMany(Memory::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function photos(): HasMany
+    {
+        return $this->hasMany(Photo::class);
     }
 }
